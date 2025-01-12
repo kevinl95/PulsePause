fe93eb00972b619bd322a91ad5dc9c79ec0033ff
kloeffler95 <kevin.loeffler@colorado.edu>
2025-01-11 19:23:45 -0700

    Adding the photoplethysmography check-in feature.

--- main.py
+++ main.py
@@ -2,12 +2,52 @@ import pystray
 import os
 import json
 import tkinter as tk
+import yarppg
+import cv2
 from PIL import Image
+import time
+import threading
 
 logo = Image.open(os.path.join("assets", "PulsePause.png"))
 
+rppg = yarppg.Rppg()
+
 disable_var = tk.BooleanVar()
 interval_var = tk.IntVar()
+
+FONT_COLOR = (0, 0, 0)
+
+def _is_window_closed(name: str) -> bool:
+    return cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) < 1
+
+def check_in():
+    if not disable_var.get():
+        cam = cv2.VideoCapture(0)
+        if not cam.isOpened():
+            print(f"Could not open camera")
+            return -1
+        tracker = yarppg.FpsTracker()
+        start_time = time.time()
+        # Run for five seconds
+        while time.time() - start_time < 5:
+            ret, frame = cam.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            if not ret:
+                break
+            result = rppg.process_frame(frame)
+            img = yarppg.roi.overlay_mask(
+                frame, result.roi.mask != 0, alpha=0.0
+            )
+            img = cv2.flip(img, 1)
+            tracker.tick()
+            result.hr = 60 * tracker.fps / result.hr
+            text = f"{result.hr:.1f} (bpm)"
+            pos = (10, img.shape[0] - 10)
+            cv2.putText(img, text, pos, cv2.FONT_HERSHEY_COMPLEX, 0.8, color=FONT_COLOR)
+            cv2.imshow("Checking stress level, please wait...", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+            print(result.value, result.hr)
+            if cv2.waitKey(1) == ord("q") or _is_window_closed("yarPPG"):
+                break
 
 def save_settings():
     global disable_var, interval_var
@@ -47,8 +87,7 @@ def open_settings():
 
 def after_click(icon, query):
     if str(query) == "Check In":
-        print("The Best Place to learn anything Tech \
-        Related -> https://www.geeksforgeeks.org/")
+        check_in()
     elif str(query) == "Settings":
         open_settings()
     elif str(query) == "Exit":
@@ -56,12 +95,20 @@ def after_click(icon, query):
         # Save settings before exiting
         save_settings()
 
+def schedule_check_in():
+    interval = interval_var.get() * 60  # Convert minutes to seconds
+    if not disable_var.get():
+        check_in()
+    threading.Timer(interval, schedule_check_in).start()
+
 icon = pystray.Icon("PP", logo, "PulsePause", 
                     menu=pystray.Menu(
-    pystray.MenuItem("Check In", 
-                     after_click),
-    pystray.MenuItem("Settings", 
-                     after_click),
-    pystray.MenuItem("Exit", after_click)))
+                        pystray.MenuItem("Check In", 
+                                         after_click),
+                        pystray.MenuItem("Settings", 
+                                         after_click),
+                        pystray.MenuItem("Exit", after_click))
+                    )
 load_settings()
+schedule_check_in()
 icon.run()
